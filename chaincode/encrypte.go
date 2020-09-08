15df6f2a15c3e15c72c76d92eab863131677ad92
moonseokchoi-kr <moonseoktech@gmail.com>
2020-09-08 12:32:18 +0900

    add new func encrypte.go

--- chaincode/encrypte.go
+++ chaincode/encrypte.go
@@ -3,23 +3,43 @@ package chaincode
 import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
-	"crypto/md5"
 	"crypto/rand"
+	"crypto/sha256"
 	"fmt"
-	"hash"
-	"io"
 
 	"github.com/btcsuite/btcutil/base58"
 	"github.com/segmentio/ksuid"
 )
 
+var msg = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+var pubkeyCurve = elliptic.P256()
+
+/**
+* make id for did
+*author : choimoonseok
+*date : 2020-09-06
+ */
 func getSpecificID() string {
 	id := ksuid.New()
 	return id.String()
 }
 
-func makeECDSAKey() (string, string, string) {
-	pubkeyCurve := elliptic.P256()
+/**
+* string to byte array
+* author : choimoonseok
+* date : 2020-09-08
+**/
+func stringTobyte(msg string) []byte {
+	return []byte(msg)
+}
+
+/**
+* created ecdsaKey
+*author : choimoonseok
+*date : 2020-09-06
+ */
+func makeECDSAKey(msg string) (string, string, string) {
+
 	privatekey := new(ecdsa.PrivateKey)
 	privatekey, err := ecdsa.GenerateKey(pubkeyCurve, rand.Reader)
 
@@ -28,10 +48,15 @@ func makeECDSAKey() (string, string, string) {
 	}
 
 	pubkey := privatekey.PublicKey
-	siginature := signHash(getSpecificID(), privatekey)
+	siginature := signHash(msg, privatekey)
 	return encodeKey(privatekey, pubkey, siginature)
 }
 
+/**
+* encode base 58 character key
+*author : choimoonseok
+*date : 2020-09-06
+ */
 func encodeKey(privateKey *ecdsa.PrivateKey, publickey ecdsa.PublicKey, signiture []byte) (string, string, string) {
 	privateByte := privateKey.D.Bytes()
 	priBase58 := base58.CheckEncode(privateByte, 0)
@@ -42,14 +67,24 @@ func encodeKey(privateKey *ecdsa.PrivateKey, publickey ecdsa.PublicKey, signitur
 	return priBase58, pubBase58, sigBase58
 }
 
+/**
+* hash to message usgin sha256
+*author : choimoonseok
+*date : 2020-09-06
+ */
+func hash(b []byte) []byte {
+	h := sha256.New()
+	h.Write(b)
+	return h.Sum(nil)
+}
+
+/**
+* created signature
+*author : choimoonseok
+*date : 2020-09-06
+ */
 func signHash(message string, privateKey *ecdsa.PrivateKey) []byte {
-	var h hash.Hash
-	h = md5.New()
-
-	io.WriteString(h, message)
-
-	signhash := h.Sum(nil)
-
+	signhash := hash(stringTobyte(message))
 	signature, serr := ecdsa.SignASN1(rand.Reader, privateKey, signhash)
 	if serr != nil {
 		fmt.Println(serr)
@@ -58,4 +93,50 @@ func signHash(message string, privateKey *ecdsa.PrivateKey) []byte {
 	return signature
 }
 
-func verifySignature(publickey string, signature string, hash []byte) {}
+/**
+* decode base 58
+*author : choimoonseok
+*date : 2020-09-06
+ */
+func decodepubkey(publickey string) ecdsa.PublicKey {
+	pubByte, _, err := base58.CheckDecode(publickey)
+	if err != nil {
+		fmt.Println(err)
+	}
+	x, y := elliptic.Unmarshal(pubkeyCurve, pubByte)
+	pubkey := ecdsa.PublicKey{
+		Curve: pubkeyCurve,
+		X:     x,
+		Y:     y,
+	}
+	return pubkey
+
+}
+
+/**
+* decode base 58 for signature
+*author : choimoonseok
+*date : 2020-09-06
+ */
+func decodeSignature(signature string) []byte {
+	sigbyte, _, err := base58.CheckDecode(signature)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	return sigbyte
+}
+
+/**
+* verify key
+*author : choimoonseok
+*date : 2020-09-06
+ */
+func verify(publickey string, signature string, message string) bool {
+	signhash := hash(stringTobyte(message))
+	pubkey := decodepubkey(publickey)
+	sig := decodeSignature(signature)
+	verify := ecdsa.VerifyASN1(&pubkey, signhash, sig)
+
+	return verify
+}
--- chaincode/encrypte_test.go
+++ chaincode/encrypte_test.go
@@ -1,6 +1,7 @@
 package chaincode
 
 import (
+	"strconv"
 	"testing"
 )
 
@@ -12,6 +13,17 @@ func TestDIDid(t *testing.T) {
 }
 
 func TestGenerateKey(t *testing.T) {
-	private, public, sig := makeECDSAKey()
-	t.Errorf("privatekey : %q, \n publickey : %q, \n siginature : %q", private, public, sig)
+	private, public, sig := makeECDSAKey("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
+	t.Logf("privatekey : %q, \n publickey : %q, \n siginature : %q", private, public, sig)
+}
+
+func TestVerify(t *testing.T) {
+	pubkey := "13n4s5tFAmoCYHLsnJ9k1nspszbuQgvjaFrmJ8cSbfLmHDGNDkc69XCExX9PpbDBLA25VK2GsvYXvXEi9xr1DWEbVfUJu8u"
+	signature := "124GJCUhBd4Nofx3bbN6aHuyjMcTeaBmtPtGar1j9iXYw1ZVzGMwXc5CGQP4nz1RBvk64sS7ecJCKdZR6YgbtSHzGcbFWfAtFqRAwjon"
+	msg := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+	got := verify(pubkey, signature, msg)
+	want := true
+	if got != want {
+		t.Errorf("verification failed! got: %q", strconv.FormatBool(got))
+	}
 }
