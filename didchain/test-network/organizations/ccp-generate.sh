12bc0b09c743226764b23c4ca58165ff76f7e1c1
moonseokchoi-kr <moonseoktech@gmail.com>
2020-11-08 21:22:32 +0900

    config

--- didchain/test-network/organizations/ccp-generate.sh
+++ didchain/test-network/organizations/ccp-generate.sh
@@ -18,6 +18,7 @@ function json_ccp {
 function json_org1_ccp {
     local PP=$(one_line_pem $6)
     local CP=$(one_line_pem $7)
+    local OP=$(one_line_pem $8)
     sed -e "s/\${ORG}/$1/" \
         -e "s/\${P0PORT}/$2/" \
         -e "s/\${P0PORT1}/$3/" \
@@ -25,11 +26,13 @@ function json_org1_ccp {
         -e "s/\${CAPORT}/$5/" \
         -e "s#\${PEERPEM}#$PP#" \
         -e "s#\${CAPEM}#$CP#" \
+        -e "s#\${PEERPEMORDER}#$OP#" \
         organizations/ccp-template-org1.json
 }
 function yaml_org1_ccp {
     local PP=$(one_line_pem $6)
     local CP=$(one_line_pem $7)
+    local OP=$(one_line_pem $8)
     sed -e "s/\${ORG}/$1/" \
         -e "s/\${P0PORT}/$2/" \
         -e "s/\${P0PORT1}/$3/" \
@@ -37,6 +40,7 @@ function yaml_org1_ccp {
         -e "s/\${CAPORT}/$5/" \
         -e "s#\${PEERPEM}#$PP#" \
         -e "s#\${CAPEM}#$CP#" \
+        -e "s#\${PEERPEMORDER}#$OP#" \
         organizations/ccp-template-org1.yaml | sed -e $'s/\\\\n/\\\n          /g'
 }
 function yaml_ccp {
@@ -57,9 +61,9 @@ P0PORT2=7071
 CAPORT=7054
 PEERPEM=organizations/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem
 CAPEM=organizations/peerOrganizations/org1.example.com/ca/ca.org1.example.com-cert.pem
-
-echo "$(json_org1_ccp $ORG $P0PORT $P0PORT1 $P0PORT2 $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/org1.example.com/connection-org1.json
-echo "$(yaml_org1_ccp $ORG $P0PORT $P0PORT1 $P0PORT2 $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/org1.example.com/connection-org1.yaml
+PEERPEMORDER=organizations/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem
+echo "$(json_org1_ccp $ORG $P0PORT $P0PORT1 $P0PORT2 $CAPORT $PEERPEM $CAPEM $PEERPEMORDER)" > organizations/peerOrganizations/org1.example.com/connection-org1.json
+echo "$(yaml_org1_ccp $ORG $P0PORT $P0PORT1 $P0PORT2 $CAPORT $PEERPEM $CAPEM $PEERPEMORDER)" > organizations/peerOrganizations/org1.example.com/connection-org1.yaml
 
 ORG=2
 P0PORT=9051
